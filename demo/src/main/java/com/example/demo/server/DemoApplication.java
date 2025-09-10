5c0fe2b64b698045f29536a767e21d2307c454ea
Rama-sab <ramah.sabboubeh@gmail.com>
2025-09-10 10:03:01 +0300

    last_v

--- demo/src/main/java/com/example/demo/server/DemoApplication.java
+++ demo/src/main/java/com/example/demo/server/DemoApplication.java
@@ -2,8 +2,14 @@ package com.example.demo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication (scanBasePackages = "com.example.demo")
+@EnableJpaRepositories(basePackages = "com.example.demo.repository")
+@EnableMongoRepositories(basePackages = "com.example.demo.repository")
+@EntityScan(basePackages = "com.example.demo.entity")
 public class DemoApplication {
 
 	public static void main(String[] args) {
